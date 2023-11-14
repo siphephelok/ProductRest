b171a06ba273541da9685c19ef9250e7e7cd9346
Siphephelo Myeni <sipheanele@gmail.com>
2023-11-15 00:29:20 +0200

    Initial commit

---
+++ ProductREST/src/test/java/Siphephelo/ProductREST/ProductRestApplicationTests.java
@@ -0,0 +1,13 @@
+package Siphephelo.ProductREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
