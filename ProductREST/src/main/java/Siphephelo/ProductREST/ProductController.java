4be2e5acc8f55f4a8de1e1f81fba3d895eb64516
Siphephelo Myeni <sipheanele@gmail.com>
2023-11-15 17:24:37 +0200

    Initialized ProductController class

---
+++ ProductREST/src/main/java/Siphephelo/ProductREST/ProductController.java
@@ -0,0 +1,17 @@
+package Siphephelo.ProductREST;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService service;
+    @GetMapping("/products")
+    public List<Product> list(){
+        return service.listAll();
+    }
+}
