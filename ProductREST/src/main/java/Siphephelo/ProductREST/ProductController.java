2be4429a8f4b474cf9c55837c203a881c05f0ba8
Siphephelo Myeni <sipheanele@gmail.com>
2023-11-16 17:03:20 +0200

    URL Mapping finished

--- ProductREST/src/main/java/Siphephelo/ProductREST/ProductController.java
+++ ProductREST/src/main/java/Siphephelo/ProductREST/ProductController.java
@@ -1,17 +1,31 @@
 package Siphephelo.ProductREST;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService service;
-    @GetMapping("/products")
+    @GetMapping("/product")
     public List<Product> list(){
         return service.listAll();
+    }
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> get(@PathVariable Integer id){
+        try {
+            Product product = service.get(id);
+            return new ResponseEntity<Product>(product, HttpStatus.OK);
+        }catch (NoSuchElementException e){
+            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+        }
     }
 }
